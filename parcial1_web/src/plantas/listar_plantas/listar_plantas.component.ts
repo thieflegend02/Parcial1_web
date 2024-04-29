2116805d10fc9d9d29ab3ad559ecc479cf613bb8
Rodrigo Bello <r.bellol@uniandes.edu.co>
2024-04-28 23:54:47 -0500

    Create module listar plantas

---
+++ parcial1_web/src/plantas/listar_plantas/listar_plantas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar_plantas',
+  templateUrl: './listar_plantas.component.html',
+  styleUrls: ['./listar_plantas.component.css']
+})
+export class Listar_plantasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ parcial1_web/src/plantas/listar_plantas/listar_plantas.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Listar_plantasComponent } from './listar_plantas.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [Listar_plantasComponent]
+})
+export class Listar_plantasModule { }
